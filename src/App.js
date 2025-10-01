90b94818259bdaae65d17b51e7d67f2b4e9756e6
jz <3274986958@qq.com>
2025-10-01 20:37:04 +0800

    feat:update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import DashboardPage from './pages/DashboardPage';
+import EditorPage from './pages/EditorPage';
+import TrashPage from './pages/TrashPage';
+import Layout from './components/Layout';
 import './App.css';
 
 function App() {
+  const [search, setSearch] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout onSearchChange={setSearch}>
+        <Routes>
+          <Route path="/" element={<DashboardPage searchTerm={search} />} />
+          <Route path="/docs/:documentId" element={<EditorPage />} />
+          <Route path="/trash" element={<TrashPage />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
